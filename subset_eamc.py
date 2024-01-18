a50cd436c7b35666f97deb57b55eae95ecfe2d44
Pranavkumar Mallela <pranavmallela2020@gmail.com>
2024-01-18 16:54:46 +0530

    added 4 cost functions

--- subset_eamc.py
+++ subset_eamc.py
@@ -84,5 +84,47 @@ def f(x, hcm_list, mpv):
 
 # cost function c
 # constant cost = 1
-def c(x):
+def c0(x):
     return sum(x)
+
+def c1(x,hcm_list):
+    cost = 0
+    x = [i for i in range(len(x)) if x[i] == 1]
+    for human in x:
+        hcm = hcm_list[human]
+        classWiseAcc = [hcm[i][i] for i, _ in enumerate(hcm)]
+        human_cost = np.sum(classWiseAcc)
+        cost+=human_cost
+    return cost
+
+def c2(x,mpv,hcm_list):
+    cost = 0
+    _, el = test_Yhm(hcm_list, mpv)
+    for i in len(x):
+        if x[i] == 1:
+            hcm = hcm_list[i]
+            classWiseAcc = hcm[el[i]][el[i]]
+            human_cost = classWiseAcc
+            cost+=human_cost
+    return cost
+
+def c3(x,mpv,hcm_list):
+    cost = 0
+    x = [i for i in range(len(x)) if x[i] == 1]
+    for human in x:
+        hcm = hcm_list[human]
+        classWiseAcc = [hcm[i][i] for i, _ in enumerate(hcm)]
+        human_cost = np.dot(classWiseAcc, mpv)
+        cost+=human_cost
+    return cost
+
+def c4(x,mpv,hcm_list):
+    cost = 0
+    x = [i for i in range(len(x)) if x[i] == 1]
+    for human in x:
+        hcm = hcm_list[human]
+        classWiseAcc = [hcm[i][i] for i, _ in enumerate(hcm)]
+        weight = [sum(hcm[y][yh] * mpv[y] for y in range(len(mpv))) for yh in range(len(mpv))]
+        human_cost = np.dot(classWiseAcc,weight)
+        cost+=human_cost
+    return cost
